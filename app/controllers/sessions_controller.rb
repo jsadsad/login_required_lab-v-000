3060e66c9ecdb5f9d0b0249f12c5eb9d2448318d
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-09-24 17:41:36 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+    redirect to '/'
+  end
+
+  def destroy
+    session.destroy :name
+  end
 end
